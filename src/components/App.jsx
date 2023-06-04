9441ef1ed206dad48a04da5526583ecf646cd534
Aleks-TO <satov69@gmail.com>
2023-06-05 00:31:10 +0300

    contactform component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework phone book1
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from './app.module.css';
+import ContactForm from './contactForm/contactForm';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  render() {
+    return (
+      <div className={css.phonebook}>
+        <h1 className={css.phonebook__title}>Phonebook</h1>
+        <ContactForm />
+        <h2 className={css.phonebook__contactsTitle}>Contacts</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
